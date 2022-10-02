7b802963cecabbc36ea77a657f46e9a438b3dc43
kernel_loophole <p190119@nu.edu.pk>
2022-10-02 16:14:39 -0400

    k-th tree

--- k-th_array_mapping.py
+++ k-th_array_mapping.py
@@ -1,21 +1,32 @@
 class TreeAncestor(object):
 
     def __init__(self, n, parent):
-        """
-        :type n: int
-        :type parent: List[int]
-        """
+    
+        self.n=n
+        self.parent=parent 
+    
         
 
     def getKthAncestor(self, node, k):
-        """
-        :type node: int
-        :type k: int
-        :rtype: int
-        """
+        print(self.parent)
+        for i in range(0,len(self.parent)):
+            if i==node:
+                if i==1:
+                    print("0")
+                else:
+                    
+                    if i-k*2<0:
+                        print("-1")
+                    print(self.parent[i],i-k)
+
+                
         
 
 
 # Your TreeAncestor object will be instantiated and called as such:
-# obj = TreeAncestor(n, parent)
-# param_1 = obj.getKthAncestor(node,k)
+n=7
+parent=[-1, 0, 0, 1, 1, 2, 2]
+node=4
+k=2
+obj = TreeAncestor(n, parent)
+param_1 = obj.getKthAncestor(node,k)

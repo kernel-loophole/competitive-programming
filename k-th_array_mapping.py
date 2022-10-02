8c81c964df737f1cd1070573b92fe289ca018c47
kernel_loophole <p190119@nu.edu.pk>
2022-10-02 16:51:29 -0400

    k0th tree

--- k-th_array_mapping.py
+++ k-th_array_mapping.py
@@ -1,12 +1,12 @@
+from traceback import print_tb
+
+
 class TreeAncestor(object):
 
     def __init__(self, n, parent):
     
         self.n=n
         self.parent=parent 
-    
-        
-
     def getKthAncestor(self, node, k):
         print(self.parent)
         for i in range(0,len(self.parent)):
@@ -15,18 +15,15 @@ class TreeAncestor(object):
                     print("0")
                 else:
                     
-                    if i-k*2<0:
+                    if i-(k*2)<0:
                         print("-1")
-                    print(self.parent[i],i-k)
-
-                
-        
-
+                    print(self.parent[i],(2*k-i))
+                    print(self.parent[2*k-i])
 
 # Your TreeAncestor object will be instantiated and called as such:
 n=7
 parent=[-1, 0, 0, 1, 1, 2, 2]
-node=4
-k=2
+node=5
+k=1
 obj = TreeAncestor(n, parent)
 param_1 = obj.getKthAncestor(node,k)

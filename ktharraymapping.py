de0c57a4f3af77c1eec0d149cdd23a083aaedda9
kernel_loophole <p190119@nu.edu.pk>
2022-10-03 12:09:21 -0400

    k th tree problem

--- ktharraymapping.py
+++ ktharraymapping.py
@@ -17,22 +17,31 @@ class TreeAncestor(object):
             if k>1:
                 # print("k",(node//2-1)-k)
                 if self.parent[node//k]<0 or (node//2-1)-k==0:
+                    
                     return (node//2-1)-k
                 else:
+                   
                     return self.parent[node//k]
             else:
-                # print("j",)
+                
+                #print("j",)
                 return self.parent[node//k],(node//2-1)+k
 
         else:
             if k>1:
+                print("l")
+                print("ma")
                 # print("l",self.parent[node//k-1]-1,(node//2-1)-(k))
                 if self.parent[node//k]<0 or (node//2-1)-k==0:
                     # print((node//2-1)-k)
+                    # print("none is none ")
                     return (node//2-1)-k
+                    
             else:
                 # print("p",self.parent[node//k],(node//2-1)+k)
+                
                 return self.parent[node//k]
+        return -1
 
 #         for i in range(0,len(self.parent)):
 #             if i==k*2:
@@ -60,8 +69,8 @@ class TreeAncestor(object):
 # # Your TreeAncestor object will be instantiated and called as such:
 n=7
 parent=[-1,0,0,1,1,2,2,3,3,4,4,5,5,6,6]
-node=8
-k=1
+node=14
+k=3
 obj = TreeAncestor(n, parent)
 param_1 = obj.getKthAncestor(node,k)
 print(param_1)

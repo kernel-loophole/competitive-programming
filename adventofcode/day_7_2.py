d14d3898a4426709897fe90b644a77d7519b5bca
kernel_loophole <p190119@nu.edu.pk>
2023-01-27 22:56:44 +0500

    day 7 advent of code

--- adventofcode/day_7_2.py
+++ adventofcode/day_7_2.py
@@ -1,9 +1,13 @@
+import re
 def recursively_find(dir_string:str,file_content:str):
     check_dir=[]
+    str_check=''
     flag=False
+    total_size_of_dir=0
     for i in file_content:
         if flag:
             check_dir.append(i)
+            str_check=str_check+i
         if flag and i.startswith('$ cd '):
             if flag==False:
                 flag=True
@@ -14,14 +18,27 @@ def recursively_find(dir_string:str,file_content:str):
             if flag==False:
                 flag=True
             else:
-                flag=False          
+                flag=False 
+    print(check_dir)         
+    if 'dir' in str_check:
+        for i in check_dir:
+            print(i)
+            if i.startswith('dir'):
+                x=re.findall('[0-9]+',i)
+                total_size_of_dir=total_size_of_dir+int(x[0])
+                print("total size of dir",total_size_of_dir)
+                print("check find")
+                dir_string=i[len(i)-2]
+                recursively_find(dir_string,file_content)
+    else:
+        return check_dir        
 def find_dir(list_string:__file__)->str:
     for i in list_string:
         try:
             if i.startswith('dir'):
                 # print(i[len(i)-2])
                 dir_string=i[len(i)-2]
-                recursively_find(dir_string,list_string)
+                print(recursively_find(dir_string,list_string))
         except:
             pass
         if i.startswith('$ cd'):
